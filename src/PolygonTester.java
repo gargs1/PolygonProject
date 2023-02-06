4d80f3f354844993dfa63f99b7a28e38a5d093f7
gargs1 <gargs@wlhs.wlwv.k12.or.us>
2023-02-06 02:38:18 -0800

    Added some lines in between each test to make code look cleaner.

--- src/PolygonTester.java
+++ src/PolygonTester.java
@@ -1,29 +1,29 @@
 public class PolygonTester {
     public static void main(String[] args) {
 
-        System.out.println("This is the output of a default polygon");
+        System.out.println("This is the output of a default polygon \n");
         Polygon defaultPolygon = new Polygon();
         System.out.println(defaultPolygon);
 
-        System.out.println("This is a polygon output with 8 sides, side length 9.832, and name octagon.");
+        System.out.println("\nThis is a polygon output with 8 sides, side length 9.832, and name octagon.\n");
         Polygon polygon1 = new Polygon(8, 9.832, "octagon");
         System.out.println(polygon1);
 
-        System.out.println("This is a polygon with 4 sides, side length 5.3, and name square");
+        System.out.println("\nThis is a polygon with 4 sides, side length 5.3, and name square\n");
         Polygon square = new Polygon(4, 5.3, "square");
         System.out.println(square);
 
-        System.out.println("This is an invalid polygon");
+        System.out.println("\nThis is an invalid polygon\n");
         Polygon invalidPoly = new Polygon(2, 1.0, "angle");
         System.out.println(invalidPoly);
 
-        System.out.println("This is an invalid polygon mutated to become valid");
+        System.out.println("\nThis is an invalid polygon mutated to become valid\n");
         Polygon polygonChanged = new Polygon(1, 3.893, "line");
         polygonChanged.setNumSides(5);
         polygonChanged.setName("pentagon");
         System.out.println(polygonChanged);
 
-        System.out.println("This is a basic test of the getter methods.");
+        System.out.println("\nThis is a basic test of the getter methods.\n");
         Polygon newPoly = new Polygon(6, 3.567, "Hexagon");
         System.out.println(newPoly.getShapeType());
         System.out.println(newPoly.getNumSides());
