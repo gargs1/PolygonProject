add78739577151ee8db38e33b819639ba15a8e86
gargs1 <gargs@wlhs.wlwv.k12.or.us>
2023-02-12 17:06:27 -0800

    I added more validation checks within the setter methods and rounded both the perimeter/area in their respective methods.

--- src/Polygon.java
+++ src/Polygon.java
@@ -15,18 +15,18 @@ import java.lang.Math;
       private int numSides;
       private double sideLength;
       private String typeShape;
-      private double Perimeter;
-      private double Area;
+      private double perimeter;
+      private double area;
 
         // default constructor
 
         /**
-         * Makes a default polygon, specifically a pentagon with 5 sides and side lengths of 8.3.
+         * Makes a default polygon, specifically a triangle with 3 sides and side lengths of 1.0.
          *
          */
         public Polygon() {
           numSides = 3;
-          sideLength = 1.365;
+          sideLength = 1.0;
           typeShape = "triangle";
         }
 
@@ -34,19 +34,26 @@ import java.lang.Math;
 
         /**
          * Makes a Polygon object with a specified sides of equal length and number of sides.
-         * If the sides are not greater than 2 and the length is not greater than 0, the polygon is considered invalid.
+         * If the sides are not greater than 2 or the length is not greater than 0, the polygon is invalid.
+         * If the polygon is invalid, the parameters are set to a default triangle with side length 1.0.
+         *
          * @param sides number of sides in the polygon
          * @param length side length of the polygon
          * @param shape name of the shape of polygon
          */
         public Polygon(int sides, double length, String shape)
         {
-          numSides = sides;
-          sideLength = length;
-          typeShape = shape;
-          if (sides <= 2 && length <= 0.0)
-          {
-              System.out.println("Not a valid polygon");
+          numSides = 3;
+          sideLength = 1.0;
+          typeShape = "Triangle";
+          if (sides <= 2 || length < 0.0) {
+            System.out.print("Not a valid polygon.");
+            System.out.println(" Your input was set to a default triangle with a side length of 1.0 unit");
+          }
+          else {
+            numSides = sides;
+            sideLength = length;
+            typeShape = shape;
           }
         }
 
@@ -66,6 +73,7 @@ import java.lang.Math;
         public double getSideLength() {
           return sideLength;
         }
+
         /**
          *
          * @return the type of shape the polygon is
@@ -74,26 +82,50 @@ import java.lang.Math;
           return typeShape;
         }
 
+        /**
+         *
+         * @return the perimeter of the polygon
+         */
+        public double getPerimeter() {
+          return perimeter;
+        }
+
+        /**
+         *
+         * @return the area of the polygon
+         */
+        public double getArea() {
+          getSideLength();
+          getNumSides();
+          calculateArea();
+          return area;
+        }
+
         // mutators
 
         /**
-         *Lets the user change the side length of the polygon.
+         *Lets the user change the side length of the polygon if new side length is greater than 0
          *
          * @param length
          */
 
         public void setSideLength(double length) {
-          this.sideLength = length;
+          if(length > 0) {
+            this.sideLength = length;
+          }
         }
 
+
         /**
-         * Lets the user change the number of sides the polygon has.
+         * Lets the user change the number of sides the polygon has if new number of sides is greater than 2.
          *
          * @param sides
          */
 
         public void setNumSides(int sides) {
-          this.numSides = sides;
+           if(sides > 2) {
+             this.numSides = sides;
+           }
         }
 
         /**
@@ -101,22 +133,25 @@ import java.lang.Math;
          *
          * @param shape
          */
-        public void setName(String shape)
+        public void setShapeName(String shape)
         {
           this.typeShape = shape;
         }
 
         public double calculatePerimeter()
         {
-          Perimeter = numSides * sideLength;
-          return Perimeter;
+          perimeter = Math.round((numSides * sideLength) * Math.pow(10, 3))/(Math.pow(10,3));
+          return perimeter;
         }
 
         public double calculateArea() {
+          calculatePerimeter();
+          getPerimeter();
           double rad = Math.toRadians(180/numSides);
           double apothem = sideLength /(2 * (Math.tan(rad)));
-          Area = .5 * apothem * Perimeter;
-          return Area;
+          double round = Math.pow(10,3);
+          area = Math.round((.5 * apothem * perimeter) * round)/round;
+          return area;
         }
 
         public String toString() {
@@ -124,9 +159,10 @@ import java.lang.Math;
           if (numSides > 2 && sideLength > 0.0) {
             DecimalFormat threeDec = new DecimalFormat("#.###");
             retMessage = "Your shape is a " + typeShape + " which has " + numSides + " sides." + "\n" +
-              "The length of each side is " + threeDec.format(sideLength) + "." + "\n" + "The total perimeter of " +
-              " your " + typeShape + " is " + threeDec.format(calculatePerimeter()) + " units. \n" + "The total area of " +
-              " your " + typeShape + " is " + threeDec.format(calculateArea()) + " units squared.";
+            "The length of each side is " + threeDec.format(sideLength) + "." + "\n" + "The total" +
+            " perimeter of your" + typeShape + " is " + threeDec.format(calculatePerimeter())
+            + " units. \n" + "The total area of " + " your " + typeShape + " is "
+            + threeDec.format(calculateArea()) + " units squared.";
           }
           return retMessage;
         }
